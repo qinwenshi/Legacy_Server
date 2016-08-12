57425f068724f8f927338b07d58ec66b847caf24
Wen <shiqinwen@wondersgroup.com>
2016-08-12 22:24:57 +0800

    throw exception in system exit stub, make sure the logic consistant with original

--- src/main/java/SystemExit.java
+++ src/main/java/SystemExit.java
@@ -2,7 +2,7 @@
  * Created by qinwenshi on 8/12/16.
  */
 public class SystemExit {
-    public void invoke(int statusCode) {
+    public void invoke(int statusCode) throws Exception {
         System.exit(1);
     }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,5 +1,7 @@
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * Created by qinwenshi on 8/12/16.
@@ -8,15 +10,23 @@ public class ServerTest {
     class SystemExitStub extends SystemExit{
         public int statusCode = -1;
         @Override
-        public void invoke(int statusCode) {
+        public void invoke(int statusCode) throws Exception {
             this.statusCode = statusCode;
+            throw new Exception("System Exit with Status " + statusCode);
         }
+
     }
+
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
 
     @Test
     public void x() throws Exception {
         SystemExitStub systemExitStub = new SystemExitStub();
         Server.setSystemExit(systemExitStub);
+        thrown.expect(Exception.class);
+        thrown.expectMessage("System Exit with Status 1");
 
         Server.main(new String[]{"a", "b", "c", "d"});;
         Assert.assertEquals(systemExitStub.statusCode, 1);
