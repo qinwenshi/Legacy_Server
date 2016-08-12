11f388f68afe5cc5fb50e8b3d955b01ed55565c1
Wen <shiqinwen@wondersgroup.com>
2016-08-12 22:15:52 +0800

    stubbing SystemExit

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,12 +1,24 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Created by qinwenshi on 8/12/16.
  */
 public class ServerTest {
+    class SystemExitStub extends SystemExit{
+        public int statusCode = -1;
+        @Override
+        public void invoke(int statusCode) {
+            this.statusCode = statusCode;
+        }
+    }
 
     @Test
     public void x() throws Exception {
+        SystemExitStub systemExitStub = new SystemExitStub();
+        Server.setSystemExit(systemExitStub);
+
         Server.main(new String[]{"a", "b", "c", "d"});;
+        Assert.assertEquals(systemExitStub.statusCode, 1);
     }
 }
