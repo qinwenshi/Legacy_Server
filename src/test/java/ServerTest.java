40c471dc235b82f812c25041b7f12b3b17640938
Wen <shiqinwen@wondersgroup.com>
2016-08-12 21:47:35 +0800

    first failure case

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,7 +1,12 @@
+import org.junit.Test;
+
 /**
  * Created by qinwenshi on 8/12/16.
  */
 public class ServerTest {
 
-
+    @Test
+    public void x() throws Exception {
+        Server.main(new String[]{"a", "b", "c", "d"});;
+    }
 }
